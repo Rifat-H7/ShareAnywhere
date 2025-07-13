033a17ff2a025da2a57c460f8e028d3f9f2e3196
Rifat <99504215+Rifat-H7@users.noreply.github.com>
2025-07-14 00:05:20 +0600

    added Text related property for File record model

--- src/ShareAnywhere/ShareAnywhere/Models/FileRecord.cs
+++ src/ShareAnywhere/ShareAnywhere/Models/FileRecord.cs
@@ -5,6 +5,8 @@
         public string Code { get; set; }
         public string FilePath { get; set; }
         public string FileName { get; set; }
+        public bool IsText { get; set; }
+        public string Text { get; set; } = string.Empty;
         public int DeleteAfterCount { get; set; } = 1;
     }
 }
